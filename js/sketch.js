a6b5ccd214c85528f56e770c47b3cc95022ae48b
Roy Vanegas <roy.vanegas@gmail.com>
2018-10-19 12:07:07 -0400

    Create a constant that represents the diameter of the eyes

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,7 @@
 const IMAGE_WIDTH = 768;
 const IMAGE_HEIGHT = 576;
 const EYE_COLOR = `#411a1f`;
+const EYE_DIAMETER = 27;
 
 function setup() {
     /* Set the dimensions of the canvas to the width and height of the image. */
