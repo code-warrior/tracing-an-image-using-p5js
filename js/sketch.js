5fe9158529bacedeff9d463b5882c455c6aa1c75
Roy Vanegas <roy.vanegas@gmail.com>
2018-10-19 12:19:47 -0400

    Create a constant that represents the color of the nose

--- js/sketch.js
+++ js/sketch.js
@@ -2,6 +2,7 @@ const IMAGE_WIDTH = 768;
 const IMAGE_HEIGHT = 576;
 const EYE_COLOR = `#411a1f`;
 const EYE_DIAMETER = 27;
+const NOSE_COLOR = `#c1857a`;
 
 function drawEyes() {
     /* Cancel the default black border around the ellipse. */
