3fbe894d1f41039d583832df23cd9d9af8801868
Roy Vanegas <roy.vanegas@gmail.com>
2018-10-19 12:06:55 -0400

    Create a constant that represents the color of the eyes

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,6 @@
 const IMAGE_WIDTH = 768;
 const IMAGE_HEIGHT = 576;
+const EYE_COLOR = `#411a1f`;
 
 function setup() {
     /* Set the dimensions of the canvas to the width and height of the image. */
