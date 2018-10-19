b1aa2e127d98daf25e51852b7bb32ee748be258e
Roy Vanegas <roy.vanegas@gmail.com>
2018-10-19 12:07:39 -0400

    Create a function that draws the eyes

--- js/sketch.js
+++ js/sketch.js
@@ -3,6 +3,20 @@ const IMAGE_HEIGHT = 576;
 const EYE_COLOR = `#411a1f`;
 const EYE_DIAMETER = 27;
 
+function drawEyes() {
+    /* Cancel the default black border around the ellipse. */
+    noStroke();
+
+    /* Set a color to fill the ellipse. */
+    fill(EYE_COLOR);
+
+    /* Draw the left eye. */
+    ellipse(305, 277, EYE_DIAMETER, EYE_DIAMETER);
+
+    /* Draw the right eye. */
+    ellipse(390, 287, EYE_DIAMETER, EYE_DIAMETER);
+}
+
 function setup() {
     /* Set the dimensions of the canvas to the width and height of the image. */
     createCanvas(IMAGE_WIDTH, IMAGE_HEIGHT);
